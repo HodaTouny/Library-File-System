3627659d9ca62133b90ab43e864461a8b73f4a62
Hoda Touny <100847847+HodaTouny@users.noreply.github.com>
2023-11-28 21:15:52 +0200

    add comment

--- IndexSystem.h
+++ IndexSystem.h
@@ -4,21 +4,10 @@
 
 
 class IndexSystem {
-private:
-public:
-//    void deleteIndexRecord(map<string, int>& index, const string& pkey) {
-//       auto it = index.find(pkey);
-//        if (it != index.end()) {
-//            int value = it->second;
-//            index.erase(it);
-//            for (auto& pair : index) {
-//                if (pair.second > value) {
-//                    pair.second++;
-//                }
-//            }
-//        }
-//    }
 
+public:
+
+   //Load Index File into vector and sort it according to the first value
     vector<pair<string, int>> LoadIndexFile(const string& filename) {
         vector<pair<string, int>> FileIndex;
         ifstream file(filename);
@@ -37,8 +26,6 @@ public:
             }
         }
         file.close();
-        file.close();
-
         sort(FileIndex.begin(), FileIndex.end(),
              [](const pair<string, int>& a, const pair<string, int>& b) {
                  return a.first < b.first;
