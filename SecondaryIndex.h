8db57ea088efa0f683a26c217e3ed0c26a54a7a1
mayahesham <96448594+mayahesham@users.noreply.github.com>
2023-12-07 17:14:13 +0200

    edit add

--- SecondaryIndex.h
+++ SecondaryIndex.h
@@ -12,6 +12,7 @@ public:
     vector<string> loadRecord(int offset,string fileName);
     void addRecordToSecondaryIndex(vector<pair<string, int>>& fileIndex,vector<pair<int,
             LinkedList<string>*>>& secondaryIndex ,string data[],int num);
+    void forPush();
 
 };
 
